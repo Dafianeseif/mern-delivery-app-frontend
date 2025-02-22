7e0a47b8fecb3487b5b7304439607717a78cd302
Dafianeseif <dafiane@fss.u-sfax.tn>
2025-02-22 23:36:52 +0100

    adding user-profile feature

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
         <Link 
         to="/" 
         className='text-3xl font-bold tracking-tight text-orange-500'>
-        RS.Delivry
+        RS.Delivery
         </Link>
         <div className='md:hidden'>
         <MobileNav/>        
