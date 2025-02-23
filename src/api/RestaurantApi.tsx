25bd2f552f812c2385636ab31d5d72b85a05804f
Dafianeseif <dafiane@fss.u-sfax.tn>
2025-02-23 23:54:04 +0100

    adding search restaurant endpoint

---
+++ src/api/RestaurantApi.tsx
@@ -0,0 +1,33 @@
+import { SearchState } from "@/pages/SearchPage";
+import { RestaurantSearcheResponse } from "@/types";
+import { useQuery } from "react-query";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useSearcheRestaurants = (searchState:SearchState,city?: string) => {
+  const createSearchRequest = async ():Promise<RestaurantSearcheResponse> => {
+    const params = new URLSearchParams();
+    params.set("searchQuery", searchState.searchQuery);
+    params.set("page", searchState.page.toString());
+    params.set("selectedCuisines", searchState.selectedCuisines.join(","));
+    params.set("sortOption", searchState.sortOption);
+    
+    const response = await fetch(
+      `${API_BASE_URL}/api/restaurant/search/${city}?${params.toString()}`
+    );
+    if (!response.ok) {
+      throw new Error("Failed to search restaurants");
+    }
+    return response.json();
+  };
+  const { data: results, isLoading } = useQuery(
+    ["searchRestaurants",searchState],
+    createSearchRequest,
+    {enabled: !!city}
+  );
+
+  return {
+    results,
+    isLoading,
+  }
+};
